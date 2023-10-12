5f0d929974cd0512fa77f19e58eff7fb903bed5a
KMG2121 <131612074+KMG2121@users.noreply.github.com>
2023-10-12 17:11:26 -0400

    Logo Swap

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
 <nav class="navbar navbar-expand-lg navbar-dark">
     <div class="container">
         <a class="navbar-brand" href="#">
-            <img src="img/Case Advantage-Logo-Transp.png" alt="Logo" width="30" height="30">
+            <img src="img/Case Advantage-2Logo - Tan BG.png" alt="Logo" width="30" height="30">
             Case Advantage
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
