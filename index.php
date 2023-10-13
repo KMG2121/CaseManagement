cc27857860d72ce0928f76af826d2b6941a68b0e
KMG2121 <131612074+KMG2121@users.noreply.github.com>
2023-10-12 20:09:35 -0400

    Remove Logo 2

--- index.php
+++ index.php
@@ -17,7 +17,6 @@
 <nav class="navbar navbar-expand-lg navbar-dark">
     <div class="container">
         <a class="navbar-brand" href="#">
-            <img src="#" alt="Logo" width="30" height="30">
             Case Advantage
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
