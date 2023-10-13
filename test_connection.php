87431866341852f88507e983ce6b7048f7ae167e
KMG2121 <131612074+KMG2121@users.noreply.github.com>
2023-10-12 21:04:54 -0400

    update conn PW

--- test_connection.php
+++ test_connection.php
@@ -1,6 +1,6 @@
 <?php
 // Include the connection.php file
-include '/home/Scorebook5734/connection.php';
+include '/home/u109918981_Scorebook5734/connection.php';
 
 // Check if the $conn object is set and not null
 if (isset($conn) && $conn) {
