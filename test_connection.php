d296c38285fa0bc2168fa88d8a02904f79f820a0
KMG2121 <131612074+KMG2121@users.noreply.github.com>
2023-10-14 12:41:07 -0400

    Update test.connection

--- test_connection.php
+++ test_connection.php
@@ -1,6 +1,6 @@
 <?php
 // Include the connection.php file
-include '/home/u109918981_Scorebook5734/connection.php';
+include '/home/u109918981/connection.php';
 
 // Check if the $conn object is set and not null
 if (isset($conn) && $conn) {
