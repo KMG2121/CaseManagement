51588a30d5cf8f3d818f55ee6abed0f0eef09382
KMG2121 <131612074+KMG2121@users.noreply.github.com>
2023-10-15 10:33:04 -0400

    bb

--- connection.php
+++ connection.php
@@ -2,11 +2,10 @@
 // File: /path/to/your/local/connection.php
 
 $credentials = [
-$servername = "localhost",  // local server name
-$username = "u109918981_Scorebook5734",  // your local db username
-$password = "qV65fyEYreAnFi",  // your local db password
-$dbname = "u109918981_CaseManagement",  // your local db name
-
+    'host' => "localhost",  // local server name
+    'username' => "u109918981_Scorebook5734",  // your local db username
+    'password' => "qV65fyEYreAnFi",  // your local db password
+    'db_name' => "u109918981_CaseManagement",  // your local db name
 ];
 
 return $credentials;
--- index.php
+++ index.php
@@ -5,7 +5,8 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 require_once('config.php');
-require_once('Database.php');
+require_once(DB_CLASS_PATH);
+
 
 $credentials = require_once(DB_CONNECTION_PATH);
 
